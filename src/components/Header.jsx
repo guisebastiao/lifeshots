41525572f2e9bdba312fe2d69fc49803d28a2d4c
Guilherme <guilhermesebastiaou.u@gmail.com>
2025-01-18 15:18:38 -0300

    tirado a borda do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Bell } from "lucide-react";
 
 export const Header = () => {
   return (
-    <section className="fixed top-0 w-full h-14 flex justify-center border-b bg-zinc-950">
+    <section className="fixed top-0 w-full h-14 flex justify-center bg-zinc-950">
       <header className="max-w-md w-full h-full flex items-center justify-between px-3">
         <h1 className="text-2xl font-bold text-zinc-50">LifeShots</h1>
         <Bell size={21} />
