0c25c55569bf38fd164b13d9e8a3998a667e5733
Guilherme <guilhermesebastiaou.u@gmail.com>
2025-01-18 11:40:43 -0300

    arrumado o erro do usuario que nao possuia story

--- src/components/ModalStory.jsx
+++ src/components/ModalStory.jsx
@@ -4,6 +4,8 @@ import { Heart, EllipsisVertical, PencilRuler, Trash } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 import { ptBR } from "date-fns/locale";
 
+import { useProfilePicture } from "@/hooks/useProfilePicture";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Dialog,
@@ -23,6 +25,9 @@ import {
 
 export const ModalStory = ({ data }) => {
   const [storyCurrent, setStoryCurrent] = useState(0);
+
+  const { getProfilePicture } = useProfilePicture();
+  const { data: profilePicture, isLoading } = getProfilePicture;
 
   const handlePrev = () => {
     if (storyCurrent > 0) {
@@ -53,99 +58,103 @@ export const ModalStory = ({ data }) => {
             data.length > 0 ? "bg-gradient" : "bg-zinc-500"
           )}>
           <Avatar className="absolute w-full h-full border-[3px] border-transparent">
-            <AvatarImage src={data[storyCurrent].author?.profilePicture} />
+            <AvatarImage src={!isLoading && profilePicture} />
             <AvatarFallback>
               <img src="/notUserPicture.png" alt="user-not-picture" />
             </AvatarFallback>
           </Avatar>
         </div>
       </DialogTrigger>
-      <DialogContent
-        className="h-full border-none px-0"
-        posClose="right-4 top-7">
-        <DialogHeader>
-          <div className="w-full h-full space-y-1.5 z-20 bg-gradient-opacity">
-            <DialogTitle />
-            <DialogDescription />
-            <div className="w-full h-[2px] flex items-center justify-center px-1 gap-1">
-              {data.map((_, i) => (
-                <span
-                  key={i}
-                  className={twMerge(
-                    "w-full h-[2px] rounded",
-                    storyCurrent >= i ? "bg-zinc-50" : "bg-zinc-400"
-                  )}
-                />
-              ))}
-            </div>
-            <div>
-              <DropdownMenu>
-                <DropdownMenuTrigger
-                  className="absolute right-12 top-[29px]"
-                  asChild>
-                  <button>
-                    <EllipsisVertical size={21} />
-                  </button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem>
-                    <PencilRuler />
-                    <span>Editar</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <Trash />
-                    <span>Excluir</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-            <div className="flex gap-2 px-3 py-1 pb-5">
-              <Avatar className="w-10 h-10">
-                <AvatarImage src={data[storyCurrent].author?.profilePicture} />
-                <AvatarFallback>
-                  <img src="/notUserPicture.png" alt="user-not-picture" />
-                </AvatarFallback>
-              </Avatar>
-              <div className="flex flex-col">
-                <span className="text-sm">
-                  {data[storyCurrent].author.username}
-                </span>
-                <span className="text-xs text-zinc-300">
-                  {formatDistance({ date: data[storyCurrent].createdAt })}
-                </span>
+      {data.length > 0 && (
+        <DialogContent
+          className="h-full border-none px-0"
+          posClose="right-4 top-7">
+          <DialogHeader>
+            <div className="w-full h-full space-y-1.5 z-20 bg-gradient-opacity">
+              <DialogTitle />
+              <DialogDescription />
+              <div className="w-full h-[2px] flex items-center justify-center px-1 gap-1">
+                {data.map((_, i) => (
+                  <span
+                    key={i}
+                    className={twMerge(
+                      "w-full h-[2px] rounded",
+                      storyCurrent >= i ? "bg-zinc-50" : "bg-zinc-400"
+                    )}
+                  />
+                ))}
+              </div>
+              <div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger
+                    className="absolute right-12 top-[29px]"
+                    asChild>
+                    <button>
+                      <EllipsisVertical size={21} />
+                    </button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem>
+                      <PencilRuler />
+                      <span>Editar</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Trash />
+                      <span>Excluir</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+              <div className="flex gap-2 px-3 py-1 pb-5">
+                <Avatar className="w-10 h-10">
+                  <AvatarImage
+                    src={data[storyCurrent].author?.profilePicture}
+                  />
+                  <AvatarFallback>
+                    <img src="/notUserPicture.png" alt="user-not-picture" />
+                  </AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                  <span className="text-sm">
+                    {data[storyCurrent].author.username}
+                  </span>
+                  <span className="text-xs text-zinc-300">
+                    {formatDistance({ date: data[storyCurrent].createdAt })}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        </DialogHeader>
-        <section className="absolute top-0 w-full h-[92%] rounded-b-lg bg-zinc-800 overflow-hidden">
-          <img
-            src={data[storyCurrent].storyImages[0].url}
-            alt="story-image"
-            className="absolute top-0 w-full h-full object-cover"
-          />
-          <div className="absolute bottom-0 w-full h-[90.5%]">
-            <button onClick={handlePrev} className="w-2/4 h-full z-20" />
-            <button
-              onClick={handleNext}
-              className="w-2/4 h-full left-1/2 z-20"
+          </DialogHeader>
+          <section className="absolute top-0 w-full h-[92%] rounded-b-lg bg-zinc-800 overflow-hidden">
+            <img
+              src={data[storyCurrent].storyImages[0].url}
+              alt="story-image"
+              className="absolute top-0 w-full h-full object-cover"
             />
-          </div>
-        </section>
-        <DialogFooter className="absolute bottom-0 w-full h-[8%]">
-          <div className="flex flex-col items-center justify-center px-2">
-            <div className="flex gap-1">
-              <Heart size={16} className="fill-red-600 stroke-red-600" />
-              <span className="text-[11px] font-medium">
-                {data[storyCurrent].amountLikes}
-              </span>
+            <div className="absolute bottom-0 w-full h-[90.5%]">
+              <button onClick={handlePrev} className="w-2/4 h-full z-20" />
+              <button
+                onClick={handleNext}
+                className="w-2/4 h-full left-1/2 z-20"
+              />
             </div>
-            <span className="text-xs text-zinc-400">Curtidas</span>
-          </div>
-          <div className="w-full flex items-center">
-            <p className="text-sm pr-2">{data[storyCurrent].content}</p>
-          </div>
-        </DialogFooter>
-      </DialogContent>
+          </section>
+          <DialogFooter className="absolute bottom-0 w-full h-[8%]">
+            <div className="flex flex-col items-center justify-center px-2">
+              <div className="flex gap-1">
+                <Heart size={16} className="fill-red-600 stroke-red-600" />
+                <span className="text-[11px] font-medium">
+                  {data[storyCurrent].amountLikes}
+                </span>
+              </div>
+              <span className="text-xs text-zinc-400">Curtidas</span>
+            </div>
+            <div className="w-full flex items-center">
+              <p className="text-sm pr-2">{data[storyCurrent].content}</p>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      )}
     </Dialog>
   );
 };
