6a2874f5a7e8b45d443c726bb8eb43aa542733e7
GuiSebastiao <guilhermesebastiaou.u@gmail.com>
2026-02-23 11:01:10 -0300

    feat: initial setup

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
+      prettier,
+    },
+    rules: {},
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
